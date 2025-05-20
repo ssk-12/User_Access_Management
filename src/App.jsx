f48c830670c0058a6c6968483dcc12b7497c4baf
Ullegadda Saisrikanta <ullegadda.srikanta@gmail.com>
2025-05-20 17:02:27 +0530

    init & tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="p-8">
+      <h1 className="text-4xl text-emerald-600 font-bold">Tailwind is working</h1>
+      <button className="bg-emerald-600 text-white px-4 py-2 rounded-md">Click me</button>
+    </div>
+  );
+}
+
+export default App;
