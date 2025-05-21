c9c93f96194eeac9ddb11af8737f907847249d1e
Ullegadda Saisrikanta <ullegadda.srikanta@gmail.com>
2025-05-22 03:26:43 +0530

    update API URL to include '/api' path

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://user-access-management-api.onrender.com';
+const API_URL = 'https://user-access-management-api.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
